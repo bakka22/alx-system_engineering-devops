a2d3c37481bf10cee5e0599bbf0c6e69fbb5b16b
bakka22 <abobakereltom@gmail.com>
2024-03-26 06:13:58 +0200

    s

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" employee TODO list progress """
+if __name__ == "__main__":
+    import json
+    import requests
+    import sys
+
+    usr_name = ""
+    task_dic = {}
+    task_count = 0
+    done_count = 0
+    usr_id = int(sys.argv[1])
+    path = str(usr_id) + ".json"
+    srch_rng = (usr_id * 20 - 19, usr_id * 20 + 1)
+    task_dic[str(usr_id)] = []
+    url = "https://jsonplaceholder.typicode.com/users/" + str(usr_id)
+    response = requests.get(url)
+    usr = response.json()
+    usr_name = usr.get("username")
+
+    for i in range(srch_rng[0], srch_rng[1]):
+        url = "https://jsonplaceholder.typicode.com/todos/" + str(i)
+        response = requests.get(url)
+        task = response.json()
+        if task.get("userId") == usr_id:
+            tmp = {}
+            tmp["task"] = task.get("title")
+            tmp["completed"] = task.get("completed")
+            tmp["username"] = usr_name
+            task_dic[str(usr_id)].append(tmp)
+    with open(path, "w") as f:
+        json.dump(task_dic, f)
