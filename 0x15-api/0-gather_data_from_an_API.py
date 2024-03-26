a4dc7a40dd2526fde244bfd34e60fa0dd653f786
bakka22 <abobakereltom@gmail.com>
2024-03-26 03:57:02 +0200

    s

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     import json
     import requests
     import sys
+
     usr_name = ""
     task_list = []
     task_count = 0
@@ -14,18 +15,18 @@ if __name__ == "__main__":
         url = "https://jsonplaceholder.typicode.com/todos/" + str(i)
         response = requests.get(url)
         task = response.json()
-        if task["userId"] == usr_id:
-            if task["completed"]:
-                task_list.append(task["title"])
+        if task.get("userId") == usr_id:
+            if task.get("completed"):
+                task_list.append(task.get("title"))
                 done_count += 1
             task_count += 1
 
     url = "https://jsonplaceholder.typicode.com/users/" + str(usr_id)
     response = requests.get(url)
     usr = response.json()
-    usr_name = usr["name"]
+    usr_name = usr.get("name")
     print(f"Employee {usr_name} is done with tasks ({done_count}/"
-          + f"task_count}):")
+          + f"{task_count}):")
     for tsk in task_list:
         print(f"\t {tsk}")
 
