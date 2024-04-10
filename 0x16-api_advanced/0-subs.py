323f0f394e57464de87d3eae62913287e84fb45d
bakka22 <abobakereltom@gmail.com>
2024-04-10 03:21:06 +0200

    S

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,8 @@ import requests
 def number_of_subscribers(subreddit):
     """ queries the Reddit API and returns the number of subscribers """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False)
+    headers = {"User-Agent": "Python Reddit API Client"}
+    response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code != 200:
         return 0
     data = response.json()
